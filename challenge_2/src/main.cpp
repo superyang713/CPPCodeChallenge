6d16798e685f805b4ae17af2aa2c8a7538c59ecd
Yang Dai <yangdai713@aim.com>
2018-07-18 16:44:16 -0400

    finished chalenge 2 and 3

---
+++ challenge_2/src/main.cpp
@@ -0,0 +1,14 @@
+/* Task
+ * Read 3 numbers from stdin and print their sum to stdout.
+*/
+
+#include <iostream>
+
+
+int main()
+{
+    int a, b ,c;
+    std::cin >> a >> b >> c;
+    std::cout << a + b + c << std::endl;
+    return 0;
+}
