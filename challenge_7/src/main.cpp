1ad39e1118b2bf217348f7290c042924dd639aa3
Yang Dai <yangdai713@aim.com>
2018-07-19 09:59:51 -0400

    finished challenge 7

--- challenge_7/src/main.cpp
+++ challenge_7/src/main.cpp
@@ -6,12 +6,18 @@ void update(int* a, int* b)
 {
     int temp = *a;
     *a = *b + temp;
-
+    *b = abs(*b - temp);
 }
 
 int main()
 {
-
+    int a, b;
+    int* pa = &a;
+    int* pb = &b;
+    std::cin >> a >> b;
+    update(pa, pb);
+    std::cout << a << std::endl;
+    std::cout << b << std::endl;
     return 0;
 }
 
